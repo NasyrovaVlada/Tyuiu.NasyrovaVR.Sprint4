0ac4407127a55066a6b73002cde87925c256f9eb
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-11-09 15:42:52 +0500

    Task5

---
+++ Tyuiu.NasyrovaVR.Sprint4.Task5.V24.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NasyrovaVR.Sprint4.Task5.V24.Lib;
+
+namespace Tyuiu.NasyrovaVR.Sprint4.Task5.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas = new int[5, 5] { { -6, 6, 4, -5, 1 },
+                                         { -5, 2, 5, -7, 6 },
+                                         { 0, -4, 3, 5, 6 },
+                                         { -1, 4, 2, 4, -5 },
+                                         { 6, 6, 0, 1, 6 } };
+            int res = ds.Calculate(mas);
+            int wait = 16;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NasyrovaVR.Sprint4.Task5.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint4.Task5.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint4.Task5.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d58e7946-1dab-4fc0-a925-50720fd04930")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
